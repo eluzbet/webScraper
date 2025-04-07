0945e7a96ad14fdb72f8b43f2577c2da1ec9ba4a
eluzbet <44121501+eluzbet@users.noreply.github.com>
2025-04-06 23:00:05 -0400

    added clearance price
    -changed url to clearance page for pc hardware
    
    -parses clearance price derived from website and prints it out, this will later be used to compare to ebay

--- src/Main.java
+++ src/Main.java
@@ -10,7 +10,7 @@ import org.jsoup.select.Elements;
 
 public class Main {
     public static void main(String[] args) {
-        String url = "https://www.microcenter.com/search/search_results.aspx?Ntk=all&sortby=match&N=4294966937&myStore=true"; // Replace with actual store URL
+        String url = "https://www.microcenter.com/search/search_results.aspx?storeid=185&Ntk=all&prt=clearance&sortby=match&N=4294966998"; // Replace with actual store URL
 
         try {
             // Fetch the webpage
@@ -27,11 +27,15 @@ public class Main {
                // priceText = priceText.replaceAll("[^0-9.]", ""); used below
                 double price = Double.parseDouble(priceText.replaceAll("[^0-9.]", ""));
 
-                String sku = product.select(".product_wrapper .sku").text();
+                String clearanceText = product.select(".price-label.compareTo").text();
+                double clearancePrice = Double.parseDouble(clearanceText.replaceAll("[^0-9.]", ""));
+
+                String sku = product.select(".sku").text();
 
                 System.out.println("Product Name: " + name);
                 System.out.println(sku);
                 System.out.println("Price: $" + price);
+                System.out.println("Clearance Price from: $" + clearancePrice);
                 System.out.println("---------------------------");
             }
         } catch (Exception e) {
